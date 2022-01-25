411dce1df8c332cf46f4f39dda1f05dcc1e20289
OM-NAKKA <94053217+OM-NAKKA@users.noreply.github.com>
2022-01-25 17:51:51 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -116,7 +116,7 @@ function createDiamonds() {
   if (World.frameCount % 320 == 0) {
        // Modify the positions of diamonds 
 
-    var diamonds = createSprite(Math.round(random(50,widht-50 ),40, 10, 10));
+    var diamonds = createSprite(Math.round(random(50,width-50 ),40, 10, 10));
     diamonds.addImage(diamondsImg);
   diamonds.scale=0.03;
   diamonds.velocityY = 5;
@@ -129,7 +129,7 @@ function createJwellery() {
   if (World.frameCount % 410 == 0) {
     //   Modify the positions of jwellery to make them spawn throughout the available screen size.
 
-    var jwellery = createSprite(Math.round(random(50, widht-50),40, 10, 10));
+    var jwellery = createSprite(Math.round(random(50, width-50),40, 10, 10));
     jwellery.addImage(jwelleryImg);
   jwellery.scale=0.13;
   jwellery.velocityY = 5;
@@ -142,7 +142,7 @@ function createSword(){
   if (World.frameCount % 530 == 0) {
     //   Modify the positions of sword to make them spawn throughout the available screen size.
 
-    var sword = createSprite(Math.round(random(50, widht-50),40, 10, 10));
+    var sword = createSprite(Math.round(random(50, width-50),40, 10, 10));
     sword.addImage(swordImg);
   sword.scale=0.1;
   sword.velocityY = 4;
